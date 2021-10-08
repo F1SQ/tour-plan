e6f613da6e60f0372bc74484ba11170c413ed7aa
F1SQ <ridchenkodenis@gmail.com>
2021-10-09 01:51:11 +0300

    create: modal and media for modal, project done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters  
   loop: true,
 
@@ -27,7 +28,31 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   // },
 });
 
-var menuButton = document.querySelector('.menu-button')
-menuButton.addEventListener('click', function(){
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+var menuButton = $('.menu-button')
+menuButton.on('click', function(){
+  $('.navbar-bottom').toggleClass('navbar-bottom--visible')
+});
+
+var modalButton = $('[data-toggle=modal]')
+var closeModalButton = $('.modal__close')
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal)
+
+
+function openModal() {
+  var targetModal = $(this).attr('data-href');
+  $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
+  $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+}
+
+function closeModal(event) {
+  event.preventDefault()
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible')
+  modalDialog.removeClass('modal__dialog--visible')
+}
+
+AOS.init();
+
 });
